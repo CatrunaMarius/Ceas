edc8fdfe2af92c68e745c2026ba8285554dd1140
Marius_haru <roberto_marius12@yahoo.com>
2020-09-04 20:05:38 +0300

    prima parte a challenge

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+
+
+function App() {
+
+  let now = new Date().toLocaleTimeString("ro-RO");
+  const [times, setTime] = useState(now)
+
+ 
+
+  function incres(){
+    const newtime = new Date().toLocaleTimeString("ro-RO");
+    setTime(newtime) 
+  }
+ 
+ 
+  
+  // console.log(time);
+
+  return (
+    <div className="container">
+      <h1>{times}</h1>
+      <button onClick={incres}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
